758c71053c9ca62ea3d6e5aba9822e47ec095424
DESKTOP-31400QB\user <paul6327931@gmail.com>
2023-01-19 11:25:29 +0800

    第一次上傳

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 50; i >= -50; i -= 10) {
+            CreateRayCast(transform.position, transform.forward * 100 + transform.right * i, Color.red);
+        }
+    }
+    void CreateRayCast(Vector3 pos, Vector3 direction, Color color)
+    {
+        Ray ray = new Ray(pos, direction);
+
+        Debug.DrawLine(pos, pos + direction, color);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10))
+        {
+            print(hit.point);
+            print(hit.transform.position);
+            print(hit.collider.gameObject);
+        }
+    }
+    
+}
