0d0e3c4a0afcb0c92a4dfb6cfcd2690d89012408
DESKTOP-31400QB\user <paul6327931@gmail.com>
2023-02-15 18:59:51 +0800

    新增存檔功能

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
+[System.Serializable]
+public class GameData
+{
+    public float player_hp;
+    public float player_mp;
+    public float player_atk;
+}
 public class DataManager : MonoBehaviour
 {
+    PlayerManager playerManager;
+    GameData gameData =  new GameData();
+    string saveFilePath;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+        saveFilePath = Application.persistentDataPath + "/gamedata.json";
     }
 
     // Update is called once per frame
@@ -16,9 +28,16 @@ public class DataManager : MonoBehaviour
         
     }
     public void SaveData()
-    { 
+    {
+        print("存檔");
+        gameData.player_atk = playerManager.Atk;
+        gameData.player_hp = playerManager.Hp;
+        gameData.player_mp = playerManager.Mp;
+        string json = JsonUtility.ToJson(gameData);
+        File.WriteAllText(saveFilePath, json);
     }
     public void LoadData()
     {
+        print("讀檔");
     }
 }
